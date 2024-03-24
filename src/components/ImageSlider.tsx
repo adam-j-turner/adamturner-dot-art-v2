beb4da894850fa5aca15160dd9a7eb8cf89bff99
Adam Turner <adamturner@protonmail.com>
2024-03-23 22:37:13 -0500

    better nav

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,13 +1,14 @@
 interface ImageSliderProps {
   paths: string[];
+  activeIdx: number;
 }
 
-export default function ImageSlider({ paths }: ImageSliderProps) {
+export default function ImageSlider({ paths, activeIdx }: ImageSliderProps) {
   const images = paths.map((path) => <img src={path} />);
 
   return (
     <>
-      <div className="image-slider">{images}</div>
+      <div className="image-slider">{images[activeIdx]}</div>
     </>
   );
 }
--- src/routes/IBiB.tsx
+++ src/routes/IBiB.tsx
@@ -1,31 +1,40 @@
+import { useState } from "react";
 import Header from "../components/Header";
 import ImageSlider from "../components/ImageSlider";
 import ProjectDescription from "../components/ProjectDescription";
+import ImageNav from "../components/ImageNav";
+
+const images = [
+  '/img/ibib/1.jpg',
+  '/img/ibib/2.jpg',
+  '/img/ibib/3.jpg',
+  '/img/ibib/4.jpg',
+  '/img/ibib/5.jpg',
+  '/img/ibib/6.jpg',
+  '/img/ibib/7.jpg',
+  '/img/ibib/8.jpg',
+  '/img/ibib/9.jpg',
+  '/img/ibib/10.jpg',
+  '/img/ibib/11.jpg',
+  '/img/ibib/12.jpg',
+  '/img/ibib/13.jpg',
+  '/img/ibib/14.jpg'
+];
 
 export default function IBiB() {
+  const [activeIdx, setActiveIdx] = useState(0);
+
   return (
     <>
       <div className="main">
-        <Header />
-        <ProjectDescription>
-          Sed ligula est, condimentum sed elit et, consequat ullamcorper ipsum. Vestibulum posuere sit amet leo nec sodales. Curabitur consectetur eget tellus ut tempor. Nulla maximus orci sed justo consectetur tincidunt. Integer pulvinar condimentum euismod. Quisque interdum quis neque quis venenatis. Etiam dapibus mattis velit at congue. Vivamus nec quam non eros dignissim vestibulum. Maecenas tincidunt commodo accumsan. Proin aliquet sem ut augue vestibulum, vitae sagittis nisl ornare. Sed vel efficitur ipsum. Etiam at arcu id elit vestibulum scelerisque. Nulla vel sagittis velit, quis suscipit arcu.
-        </ProjectDescription>
-        <ImageSlider paths={[
-          '/img/ibib/1.jpg',
-          '/img/ibib/2.jpg',
-          '/img/ibib/3.jpg',
-          '/img/ibib/4.jpg',
-          '/img/ibib/5.jpg',
-          '/img/ibib/6.jpg',
-          '/img/ibib/7.jpg',
-          '/img/ibib/8.jpg',
-          '/img/ibib/9.jpg',
-          '/img/ibib/10.jpg',
-          '/img/ibib/11.jpg',
-          '/img/ibib/12.jpg',
-          '/img/ibib/13.jpg',
-          '/img/ibib/14.jpg'
-        ]} />
+        <div className="nav-desc">
+          <Header />
+          <ProjectDescription>
+            Sed ligula est, condimentum sed elit et, consequat ullamcorper ipsum. Vestibulum posuere sit amet leo nec sodales. Curabitur consectetur eget tellus ut tempor. Nulla maximus orci sed justo consectetur tincidunt. Integer pulvinar condimentum euismod. Quisque interdum quis neque quis venenatis. Etiam dapibus mattis velit at congue. Vivamus nec quam non eros dignissim vestibulum. Maecenas tincidunt commodo accumsan. Proin aliquet sem ut augue vestibulum, vitae sagittis nisl ornare. Sed vel efficitur ipsum. Etiam at arcu id elit vestibulum scelerisque. Nulla vel sagittis velit, quis suscipit arcu.
+          </ProjectDescription>
+          <ImageNav activeIdx={activeIdx} setActiveIdx={setActiveIdx} imagesLen={images.length} />
+        </div>
+        <ImageSlider paths={images} activeIdx={activeIdx} />
       </div>
     </>
   )
