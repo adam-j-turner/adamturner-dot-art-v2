ac4adf4aaadc3a83c3af55eb1c9d2118d701ca37
Adam Turner <adamturner@protonmail.com>
2024-03-26 13:56:14 -0500

    add home image

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -2,12 +2,14 @@ import { isMobile } from "react-device-detect";
 
 interface ImageSliderProps {
   paths: string[];
+  extraLarge?: boolean;
 }
 
-export default function ImageSlider({ paths }: ImageSliderProps) {
+export default function ImageSlider({ paths, extraLarge=false }: ImageSliderProps) {
   const images = paths.map((path) => <img src={path} />);
 
   let className = isMobile ? "image-slider-mobile" : "image-slider";
+  if (extraLarge) { className += " image-slider-xl"}
 
   return (
     <>
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
+import ImageSlider from "../components/ImageSlider";
 
 export default function Home() {
   return (
     <div className="main">
       <Header />
+      <ImageSlider extraLarge={true} paths={["/img/home/1.jpg"]} />
     </div>
   );
 }
