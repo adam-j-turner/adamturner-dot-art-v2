8c6e3b7aca317b5625566f0c7d786e8153150564
Adam Turner <adamturner@protonmail.com>
2025-05-13 10:49:34 -0500

    email

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -8,8 +8,11 @@ export default function About() {
         <div className="nav-desc">
           <Header />
           <ProjectDescription>
-            Adam Turner (b. 1994, Missouri) is a photographer and artist working primarily in the Midwestern United States. He intends to blur the lines between fiction vs. nonfiction, staged vs. candid, and constructed vs. found.
+            Adam Turner (b. 1994, Missouri) is a photographer and artist working primarily in the Midwestern United States. He has recently participated in Magnum's hybrid long-term workshop on visual storytelling, and is a 2025 participant in Penumbra Foundation's Long Term Program (LTP). As a visual storyteller, he intends to blur the lines between fiction and nonfiction, and to challenge the viewer's inherent preconceptions of photography.
           </ProjectDescription>
+          <p className="email">
+            adamturner@protonmail.com
+          </p>
         </div>
       </div>
     </>
